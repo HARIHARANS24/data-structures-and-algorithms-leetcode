e934c900fcd4ec4316131879b7345ebf2b08cdff
HARIHARAN S <hariharan24hs@gmail.com>
2026-03-16 10:47:12 +0530

    Time: 11 ms (98.34%) | Memory: 46.7 MB (98.82%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<>();
+        if (s.length() == 0 || words.length == 0) return res;
+
+        int wordLen = words[0].length();
+        int totalWords = words.length;
+        int totalLen = wordLen * totalWords;
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) map.put(w, map.getOrDefault(w, 0) + 1);
+
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, count = 0;
+            Map<String, Integer> window = new HashMap<>();
+
+            for (int j = i; j + wordLen <= s.length(); j += wordLen) {
+                String word = s.substring(j, j + wordLen);
+
+                if (map.containsKey(word)) {
+                    window.put(word, window.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (window.get(word) > map.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        window.put(leftWord, window.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == totalWords) {
+                        res.add(left);
+                        String leftWord = s.substring(left, left + wordLen);
+                        window.put(leftWord, window.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+                } else {
+                    window.clear();
+                    count = 0;
+                    left = j + wordLen;
+                }
+            }
+        }
+
+        return res;
+    }
+}
