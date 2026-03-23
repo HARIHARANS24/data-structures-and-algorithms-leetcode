3df8300d51496497c5052d30b185352e67919326
HARIHARAN S <hariharan24hs@gmail.com>
2026-03-23 13:26:46 +0530

    Time: 5 ms (98.33%) | Memory: 43.7 MB (91.52%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] sMap = new int[256], tMap = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char sc = s.charAt(i), tc = t.charAt(i);
+            if (sMap[sc] != tMap[tc]) return false;
+            sMap[sc] = tMap[tc] = i + 1;
+        }
+        return true;
+    }
+}
