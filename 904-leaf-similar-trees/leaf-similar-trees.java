6e5ab220103b8e61898d6fc242fb061ecabea429
HARIHARAN S <hariharan24hs@gmail.com>
2026-03-26 13:50:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (79.23%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        dfs(root1, l1);
+        dfs(root2, l2);
+        return l1.equals(l2);
+    }
+
+    private void dfs(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) {
+            list.add(node.val);
+            return;
+        }
+        dfs(node.left, list);
+        dfs(node.right, list);
+    }
+}
