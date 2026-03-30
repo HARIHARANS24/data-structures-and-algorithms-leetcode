6d2d6eb9654cca6a4d73560309a7470edc2b28d5
HARIHARAN S <hariharan24hs@gmail.com>
2026-03-30 13:37:10 +0530

    Time: 1 ms (84.30%) | Memory: 42.7 MB (79.83%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> strStack = new Stack<>();
+        StringBuilder current = new StringBuilder();
+        int k = 0;
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                k = k * 10 + (c - '0');
+            } else if (c == '[') {
+                countStack.push(k);
+                strStack.push(current);
+                current = new StringBuilder();
+                k = 0;
+            } else if (c == ']') {
+                int repeat = countStack.pop();
+                StringBuilder prev = strStack.pop();
+                for (int i = 0; i < repeat; i++) {
+                    prev.append(current);
+                }
+                current = prev;
+            } else {
+                current.append(c);
+            }
+        }
+
+        return current.toString();
+    }
+}
