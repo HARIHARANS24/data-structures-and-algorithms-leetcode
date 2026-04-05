0fc9f4e42aa10c6e4353018fcb56b59f68910120
HARIHARAN S <hariharan24hs@gmail.com>
2026-04-05 13:18:08 +0530

    Time: 54 ms (14.86%) | Memory: 146.7 MB (78.07%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length, m = potions.length;
+        int[] res = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = m - 1;
+            int idx = m;
+            
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if ((long) spells[i] * potions[mid] >= success) {
+                    idx = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            
+            res[i] = m - idx;
+        }
+        
+        return res;
+    }
+}
