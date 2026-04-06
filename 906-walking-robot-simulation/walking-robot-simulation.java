0bed56eb47d1ad97ba12d819c312c514207a4dbd
HARIHARAN S <hariharan24hs@gmail.com>
2026-04-06 12:16:39 +0530

    Time: 35 ms (52.90%) | Memory: 51.8 MB (77.90%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> set = new HashSet<>();
+        for (int[] o : obstacles) {
+            set.add(o[0] + "," + o[1]);
+        }
+        
+        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+        int dir = 0, x = 0, y = 0, max = 0;
+        
+        for (int c : commands) {
+            if (c == -1) {
+                dir = (dir + 1) % 4;
+            } else if (c == -2) {
+                dir = (dir + 3) % 4;
+            } else {
+                for (int i = 0; i < c; i++) {
+                    int nx = x + dirs[dir][0];
+                    int ny = y + dirs[dir][1];
+                    if (set.contains(nx + "," + ny)) break;
+                    x = nx;
+                    y = ny;
+                    max = Math.max(max, x * x + y * y);
+                }
+            }
+        }
+        
+        return max;
+    }
+}
