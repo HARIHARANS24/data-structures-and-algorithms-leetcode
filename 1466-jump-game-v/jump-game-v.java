c762dd8936473f62911d828583e0a35d6a975d6b
HARIHARAN S <hariharan24hs@gmail.com>
2026-05-24 11:39:47 +0530

    Time: 9 ms (97.43%) | Memory: 46.5 MB (30.29%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.java
@@ -0,0 +1,39 @@
+class Solution {
+    int[] dp;
+    int[] arr;
+    int d, n;
+
+    public int maxJumps(int[] arr, int d) {
+        this.arr = arr;
+        this.d = d;
+        n = arr.length;
+
+        dp = new int[n];
+
+        int ans = 1;
+
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dfs(i));
+        }
+
+        return ans;
+    }
+
+    int dfs(int i) {
+        if (dp[i] != 0) return dp[i];
+
+        int best = 1;
+
+        for (int j = i - 1; j >= Math.max(0, i - d); j--) {
+            if (arr[j] >= arr[i]) break;
+            best = Math.max(best, 1 + dfs(j));
+        }
+
+        for (int j = i + 1; j <= Math.min(n - 1, i + d); j++) {
+            if (arr[j] >= arr[i]) break;
+            best = Math.max(best, 1 + dfs(j));
+        }
+
+        return dp[i] = best;
+    }
+}
